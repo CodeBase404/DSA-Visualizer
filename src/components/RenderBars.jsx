0722dc1fccf9ad08cd3737e854f9b0dea456833d
Sonu Parvez <92024975+CodeBase404@users.noreply.github.com>
2025-03-12 10:28:07 +0530

    Update RenderBars.jsx

--- src/components/RenderBars.jsx
+++ src/components/RenderBars.jsx
@@ -1,8 +1,21 @@
 import { motion } from "motion/react";
 import { useState } from "react";
 
-function RenderBars({ arr, steps, currentStep,raceMode,algorithm }) {
-  const current = steps[currentStep] || { array: arr, compare: [], swap: [], sorted: [] }
+function RenderBars({ arr, steps, currentStep,raceMode,algorithm,algorithmType, searchTarget }) {
+  const current = steps[currentStep] || {
+    array: arr,
+    compare: [],
+    swap: [], 
+    sorted: [],
+  ...(algorithmType ==="searching" ? {
+    current:-1,
+    found:-1,
+    left:-1,
+    right:-1,
+    mid:-1,
+    target:searchTarget
+  }:{})
+  }
   const [viewMode, setViewMode] = useState("bars");
 
   const barWidth = 42;
@@ -14,21 +27,49 @@ function RenderBars({ arr, steps, currentStep,raceMode,algorithm }) {
   const scale = baseHeight / maxValue
 
   const getColor = (index, current) => {
-    if (current.sorted.includes(index)) {
-      return "#16e53c";
-    }
-    if (current.swap.includes(index)) {
-      return "orange";
-    }
-    if (current.compare.includes(index)) {
-      return "#ff1c1ce9";
-    }
+    if(algorithmType ==="searching"){
+            if(current.found === index) return "#16e53c";
+            if(current.current === index) return "#ff1c1ce9";
+            if(current.mid === index) return "orange";
+            if(current.left <= index & index <= current.right) return "purple";
+            return "steelblue";
+        }
+    else{
+    if (current.sorted?.includes(index)) return "#16e53c";
+    if (current.swap?.includes(index)) return "orange";
+    if (current.compare?.includes(index)) return "#ff1c1ce9";
     return "steelblue";
   }
+};
+  
+    const isSearchComplete = currentStep === steps.length - 1;
+    const isFound = current.found !== -1;
 
   return (
     <div className="relative flex flex-col justify-center items-center">
       <button className={`shadow mt-2 active:scale-90 shadow-gray-500 font-semibold text-green-700 hover:text-yellow-600 hover:shadow-gray-400 ${algorithm.length>=2?" md:mt-1 absolute top-[-30px] right-0 md:top-0 md:relative py-1 mt-1 md:px-4 md:py-2 px-2 text-[10px]":"px-4 py-2"}  rounded-2xl cursor-pointer`} onClick={() => setViewMode(viewMode === "bars" ? 'box' : 'bars')}>{viewMode==='bars'?'Box':'Bars'}</button>
+
+            {algorithmType === "searching" && isSearchComplete && (
+                <motion.div
+                initial={{opacity:0, scale:0.5}}
+                animate={{opacity:1,scale:1}}
+                transition={{duration:0.5,ease:"easeOut"}}
+                className="mt-2 shadow shadow-gray-500 p-1 text-sm lg:text-xl font-semibold text-white">
+                {isFound?"✅ Element Found 🎉😊":"❌Element Not Found🥲"}
+                </motion.div>
+            )}
+
+      
+            {algorithmType === "sorting" && currentStep === steps.length - 1 && (
+                <motion.div
+                initial={{opacity:0, scale:0.5}}
+                animate={{opacity:1,scale:1}}
+                transition={{duration:0.5,ease:"easeOut"}}
+                className={`mt-2 shadow shadow-gray-500 p-1 ${algorithm.length >= 2 ? "lg:text-[10px]" : ""} text-[10px] mx-4 lg:text-xl font-semibold text-white`}>
+               ✅ Sorting Completed 🎉 Sorted Array : [{current.array.join(", ")}]
+                </motion.div>
+            )}
+      
       <svg className={`p-2 w-full ${algorithm.length==2?"flex-row h-[20vh]":"flex-col"} ${raceMode && algorithm.length==3?"h-[15vh]  md:h-[50vh] lg:h-[60vh] ":"h-[33vh] md:h-[55vh] lg:h-[60vh]"} `}
         viewBox={`0 0 ${(arr.length * (viewMode === "bars" ? barSpacing : (boxSize + gap)))} ${baseHeight + 50}`}
       >
@@ -82,9 +123,20 @@ function RenderBars({ arr, steps, currentStep,raceMode,algorithm }) {
         }
       </svg>
       <ul className="absolute bottom-2 text-white flex justify-center gap-2 text-[4px] sm:left-5 sm:text-[9px] left-3 lg:left-5 lg:gap-10 md:text-[9px]">
+        {algorithmType === "searching" ? (
+      <>
+       <li>🔴 Current</li>
+        <li>🟠 Mid</li>
+        <li>🟣 Range</li>
+        <li>🟢 Found</li>
+      </>
+        ) : (
+      <>
         <li>🔴 Compared</li>
         <li>🟠 Swapped</li>
         <li>🟢 Sorted</li>
+      </>
+      )}
       </ul>
     </div>
   )
