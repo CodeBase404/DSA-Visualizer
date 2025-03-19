b5002ad61fc6219a89f9728e97542bb154880da9
Sonu Parvez <92024975+CodeBase404@users.noreply.github.com>
2025-03-19 20:38:47 +0530

    Update Visualizer.jsx

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -169,7 +169,7 @@ function Visualizer({ algorithm, setAlgorithm}) {
                         {algorithmType === "array" && <Array list={list} setList={setList} />}
                         {(algorithmType === "sorting" || algorithmType === "searching") && (
                             raceMode ? (
-                            <div className={`flex lg:mt-5 lg:flex-row ${algorithm.length == 2 ? "flex-row  mt-20" : "flex-col"} text-white gap-1 m-1 md:gap-2`}>
+                            <div className={`flex lg:mt-5 lg:flex-row ${algorithm.length == 2 ? "flex-col" : "flex-col"} text-white gap-1 m-1 md:gap-2`}>
                                 {algorithm.map((algo) => (
                                     <div key={algo} className="border p-2 rounded-2xl border-gray-300">
                                         <h3 className={`text-sm md:text-md lg:text-xl md:text-center`}>
@@ -191,7 +191,7 @@ function Visualizer({ algorithm, setAlgorithm}) {
                     )}
                 </div>
             </div>
-             <div className={`md:block lg:hidden ${raceMode?"md:hidden":""} md:w-full lg:max-w-80 h-[99%] rounded-2xl`}>
+             <div className={`md:block mb-25 p-1 lg:hidden ${raceMode?"md:hidden":""} md:w-full lg:max-w-80 h-[99%] rounded-2xl`}>
                     {!raceMode && algorithm.length > 0 && (
                         <Pseudocodecopy algorithm={algorithm[0]} />
                     )}
