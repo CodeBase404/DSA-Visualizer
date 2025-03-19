157cbebff64d13a87ff816bd55e6d3356f56f85e
Sonu Parvez <92024975+CodeBase404@users.noreply.github.com>
2025-03-19 10:54:35 +0530

    Update Controller.jsx

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -255,37 +255,37 @@ function Controller({ input, setInput, speed, setSpeed, isPlaying, setIsPlaying,
           <RotateCcw className='size-10 text-pink-400 active:text-pink-500 cursor-pointer' />
         </button>
       </div>
-      {winner && raceMode && <motion.button whileTap={{scale:0.5}}  dragConstraints={{top:-190, bottom:250, left:-320, right:0}} drag onClick={() => setShowLeaderboard(!showLeaderboard)} className="shadow-2xl shadow-yellow-200 bg-white md:hidden rounded-full pr-6 fixed top-60 z-15 right-6 h-15 w-15">
+      {winner && raceMode && <motion.button whileTap={{scale:0.5}}  dragConstraints={{top:-350, bottom:120, left:-300, right:0}} drag onClick={() => setShowLeaderboard(!showLeaderboard)} className="shadow-2xl shadow-yellow-200 bg-white md:hidden rounded-full pr-6 fixed top-100 z-15 right-5 h-15 w-15">
         {showLeaderboard?<X className='h-10 w-10 ml-2'/>:<LayoutDashboard className='h-10 w-10 ml-2' />}
       </motion.button>}
       {
-        winner && raceMode && (
+        raceMode && (
           <motion.div
             initial={false}
             animate={{ y: showLeaderboard ? 450 : 0 }}
             transition={{ duration: 0.7, ease: 'anticipate' }}
-            className='mt-20 fixed md:relative bg-[#3c0318f1] backdrop-blur-2xl md:bg-transparent w-[90%] top-[-60%] md:top-0 md:w-[100%] z-5 rounded-lg lg:mt-5'>
+            className='mt-20 fixed md:relative  w-[90%] top-[-60%] md:top-0 md:w-[100%] z-5 rounded-lg lg:mt-5'>
               {
-                <div className='shadow-gray-400 shadow-md p-2 rounded-lg text-white w-[100%]'>
+                <div className='shadow-gray-400 bg-white text-black shadow-md p-2 rounded-lg w-[100%]'>
                     <h2 className='text-2xl md:text-xl font-bold mb-2'>🏆Leaderboard</h2>
                     {
                       winner && (
-                        <div className='shadow-md shadow-purple-300 text-white md:text-[12px] lg:text-[95%] p-2 rounded-lg text-center mb-2 font-bold'>
+                        <div className='shadow-md shadow-purple-300  md:text-[12px] lg:text-[95%] p-2 rounded-lg text-center mb-2 font-bold'>
                           Winner : <strong className='text-green-500 font-extrabold'>🥇{winner}</strong>
                         </div>
                       )
                     }
                     <table className='w-full'>
                       <thead>
-                        <tr className='border-b'>
+                        <tr>
                           <th className='text-left p-1'>Algorithms</th>
                         </tr>
                       </thead>
                       <tbody>
                         {
                           algorithm.map((algo)=>(
-                            <tr key={algo} className={`border-t ${algo===winner?"bg-green-600 font-semibold  text-white":""}`}>
-                                <td className={`${algorithm.length<=3?"py-4 px-2 text-[0.8rem] md:text-[0.8rem]":"md:text-[0.8rem]"} md:p-2 lg:p-[0.6rem]`}>
+                            <tr key={algo} className={`${algo===winner?"bg-green-600 font-semibold text-white":""}`}>
+                                <td className={`${algorithm.length<=3?"py-4 px-2 text-[0.8rem] md:text-[0.8rem] rounded-md ":"md:text-[0.8rem]"} md:p-2 lg:p-[0.6rem] rounded-md `}>
                                   {algo}
                                 </td>
                             </tr>
